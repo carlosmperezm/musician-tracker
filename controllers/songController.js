4c7232dbe5d5fae994af4782f0f1adf72c46e25c
CarlosPerez <carlosmperez2901@gmail.com>
2025-12-18 16:19:52 -0800

    Show tone's name instead of their ID in the songs page

--- controllers/songController.js
+++ controllers/songController.js
@@ -1,6 +1,11 @@
 import * as db from "../db/queries.js";
 
 export async function getAllSongs(req, res) {
-    const songs = await db.getAllSongs();
-    return res.render("songsDetails", { songs })
+    const rows = await db.getAllSongs();
+    const songs = [];
+    for (const song of rows) {
+        const tone = await db.getToneById(song.tone);
+        songs.push({ ...song, tone: tone.name });
+    }
+    return res.render("songsDetails", { songs });
 }
