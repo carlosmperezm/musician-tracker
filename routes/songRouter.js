81a76ccef85fd4396daf66cbe82b2d651b7da456
CarlosPerez <carlosmperez2901@gmail.com>
2025-12-23 13:36:24 -0800

    Authenticate as admin before deleting or updating any song

--- routes/songRouter.js
+++ routes/songRouter.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { createSong, deleteSong, getAllSongs, getSongForm, updateSong } from "../controllers/songController.js";
+import { authenticate } from "../controllers/auth.js";
 
 const songRouter = Router();
 songRouter.get("/", getAllSongs);
 
 songRouter.get("/new", getSongForm);
 songRouter.post("/new", createSong, redirectToSongs);
-songRouter.get("/delete/:songId", deleteSong, redirectToSongs);
-songRouter.get("/edit/:songId", updateSong);
+songRouter.post("/delete/:songId", authenticate, deleteSong, redirectToSongs);
+songRouter.post("/edit/:songId", authenticate, updateSong);
 songRouter.post("/new/:songId", deleteSong, createSong, redirectToSongs);
 
 
