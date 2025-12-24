986dc44aca5cf494d32b5dbd2e905003b062d24e
CarlosPerez <carlosmperez2901@gmail.com>
2025-12-23 20:32:07 -0800

    Add styles to the navbar

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const PORT = 3000;
 app.set("views", path.join(import.meta.dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.use("/", appRouter)
 
