7020238befb27b46d71387eb0cbd32292870ce1e
CarlosPerez <carlosmperez2901@gmail.com>
2025-12-27 16:39:29 -0800

    Fix: form picture not showing on landscape view

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,7 +5,6 @@ export async function authenticate(req, res, next) {
         req.body.username !== process.env.USERNAME ||
         req.body.password !== process.env.PASSWORD
     ) {
-        // TODO: print a button so they can go back or go to home again
         return res.send("Wrong admin credentials");
     }
     return next();
